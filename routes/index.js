51e9ec915d43e79b8d8ef0c3373a1955e7303a67
Dave Laxman <davelaxman@gmail.com>
2017-03-20 22:46:17 -0600

    Added in blues demo

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Learning to Code' });
+});
+
+/* GET blues link page. */
+router.get('/blues', function(req, res, next) {
+  res.render('bluesDemo', { title: 'Blues Demo' });
 });
 
 module.exports = router;
